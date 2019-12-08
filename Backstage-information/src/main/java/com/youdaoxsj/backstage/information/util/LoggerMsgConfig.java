8d6a62ebcd052debb3a4e554c6a7cd6115489ac9
nagi30002 <jiuxiaakagi@outlook.com>
2019-12-08 23:55:47 +0800

    191208 增加公众号司机查询功能

---
+++ Backstage-information/src/main/java/com/youdaoxsj/backstage/information/util/LoggerMsgConfig.java
@@ -0,0 +1,18 @@
+package com.youdaoxsj.backstage.information.util;
+
+import lombok.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+
+public class LoggerMsgConfig {
+
+}
